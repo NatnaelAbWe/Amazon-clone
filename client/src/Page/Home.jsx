b864ac572d172e0ec0b9b9989c2289e02f2e60e3
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-11-13 15:58:10 +0300

    feat: amazone navbar

---
+++ client/src/Page/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../Component/Header";
+import Hero from "../Component/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
