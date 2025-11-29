53de414850f136c6c85f7085faa40aa9573bfb7f
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-11-29 20:25:07 +0300

    feat: intializing firbase

--- client/src/Utility/firebase.js
+++ client/src/Utility/firebase.js
@@ -1,20 +1,13 @@
-import firebase from "firebase/compat/app"; // Import the compat version of Firebase
+import { initializeApp } from "firebase/app";
 
-import { getAuth } from "firebase/auth";
-import "firebase/compat/firestore"; // Import Firestore compat
-import "firebase/compat/auth"; // Import Auth compat
-
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCmm2U0EcRuJYvLeU-8i168R1qoSl20_ME",
-  authDomain: "clone-evangadi-2024.firebaseapp.com",
-  projectId: "clone-evangadi-2024",
-  storageBucket: "clone-evangadi-2024.appspot.com",
-  messagingSenderId: "541618558773",
-  appId: "1:541618558773:web:bb870fc732ace2cc951e7c",
+  apiKey: "AIzaSyCZXepOlV8uUqAtj_kxCTP1VY2HPPzLmRs",
+  authDomain: "e-clone-dd69e.firebaseapp.com",
+  projectId: "e-clone-dd69e",
+  storageBucket: "e-clone-dd69e.firebasestorage.app",
+  messagingSenderId: "348058234140",
+  appId: "1:348058234140:web:cefb17d761cdb27b965ed9",
 };
 
-// Initialize Firebase using the compat version
-const app = firebase.initializeApp(firebaseConfig);
-export const auth = getAuth(app); // Use Firebase compat Auth
-export const db = firebase.firestore(); // Use Firebase compat Firestore
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
