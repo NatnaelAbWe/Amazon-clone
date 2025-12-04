ae932efd723907ae7667cd7c8a79218921b72d4c
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-12-04 21:05:32 +0300

    fix:context

--- client/src/Component/DataProvider.jsx
+++ client/src/Component/DataProvider.jsx
@@ -1,10 +1,26 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Utility/firebase";
+import { Type } from "../Utility/action.type";
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children, reducer, initialState }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      dispatch({
+        type: Type.SET_USER,
+        user: user ? user : null,
+      });
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <DataContext.Provider value={useReducer(reducer, initialState)}>
+    <DataContext.Provider value={[state, dispatch]}>
       {children}
     </DataContext.Provider>
   );
