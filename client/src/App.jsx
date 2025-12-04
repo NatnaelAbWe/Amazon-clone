a7f0c2e78113810d5544266f40aee07c53c61748
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-12-04 21:22:57 +0300

    feat: auth state fetch from firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
 import "./App.css";
 import Home from "./Page/Home";
+import { useContext, useEffect } from "react";
 import Routing from "./Router";
+import { DataContext } from "./Component/DataProvider";
+import { auth } from "./Utility/firebase";
+import { Type } from "./Utility/action.type";
 
 function App() {
+  const [dispatch] = useContext(DataContext);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: Type.SET_USER,
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: Type.SET_USER,
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]);
   return (
     <>
       <Routing />
